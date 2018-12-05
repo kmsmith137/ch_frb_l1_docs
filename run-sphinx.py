6229c8445677071b3210674710dc6b930b9b3abc
Kendrick Smith <kmsmith@perimeterinstitute.ca>
2018-12-04 22:25:36 -0500

    factor run-sphinx.py into sphinx_helpers.run_sphinx()

--- run-sphinx.py
+++ run-sphinx.py
@@ -13,75 +13,9 @@
 
 
 import os
-import sys
+import sphinx_helpers
 
+toplevel_dir = os.path.dirname(os.path.realpath(__file__))
+print 'Running sphinx in directory', toplevel_dir
 
-####################################################################################################
-#
-# Sanity checks
-
-
-script_dir = os.path.dirname(os.path.realpath(__file__))
-
-if os.getcwd() != script_dir:
-    print >>sys.stderr, "run-sphinx.py: to avoid confusion, the current working directory must be the same as the directory containing the script"
-    print >>sys.stderr, "    (cwd='%s', script_dir='%s')" % (os.getcwd(), script_dir)
-    sys.exit(1)
-
-#for f in [ "sphinx/index.rst", "docs/index.html" ]:
-#    if not os.path.exists(f):
-#        print >>sys.stderr, "run-sphinx.py: file '%s' does not exist ?!" % f
-#        sys.exit(1)
-
-
-####################################################################################################
-
-
-def xsystem(cmd, show=True):
-    if show:
-        print '+', cmd
-
-    if os.system(cmd):
-        print >>sys.stderr, "Shell command failed:", cmd
-        sys.exit(1)
-
-
-# The -E flag forces 'sphinx-build' to reread all its input files, even if nothing
-# appears to have changed.  This is necessary because 'sphinx-build' has no way of
-# knowing if docstrings have been changed since last time.
-
-xsystem("sphinx-build -E -b html sphinx docs")
-
-
-# The only thing I don't like about the RTD Sphinx theme is that it doesn't put any
-# spacing between bullet points in unordered lists.  I got carried away and wrote
-# this code to hack the Sphinx-generated CSS!
-
-with open('docs/_static/css/theme.css', 'a') as f:
-    maxdepth = 9
-    print >>f, "ul li { margin: 10px 0; }"
-    for d in xrange(1, maxdepth+1):
-        print >>f, "li.toctree-l%d { margin: 0 0; }" % d
-
-
-print
-print 'A local copy of the documentation has been built successfully!'
-print 'To view it, point your web browser here:'
-print
-print '    file://%s' % os.path.join(os.getcwd(), 'docs/index.html')
-
-# Traverse 'docs' directory looking for missing .nojekyll files.
-# My understanding is that every directory which contains subdirectories must contain .nojekyll.
-
-dirs_already_checked = set([''])
-
-def check_nojekyll(opaque_arg, dirname, fnames):
-    if dirname.startswith('docs/.doctrees') or dirname.startswith('docs/.buildinfo'):
-        return
-    parent = os.path.dirname(dirname)
-    if parent not in dirs_already_checked:
-        if not os.path.exists(os.path.join(parent, '.nojekyll')):
-            print "*** WARNING: directory '%s' does not contain the .nojekyll file (required by Github Pages)" % parent
-        dirs_already_checked.add(parent)
-
-os.path.walk('docs', check_nojekyll, None)
+sphinx_helpers.run_sphinx(toplevel_dir)
